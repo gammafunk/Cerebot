gammafunk/Cerebot

commit 448b7bb389c9287c3cbe0e00835e5acf542feffd

fix: Filter out ordered list markdown
Recently discord seems to have changed its markdown parsing to
recognized ordered lists. This messes up Sequell's listgame output,
since that often begins with a number followed by a period. The
discord.py module's `utils.escape_markdown()` function apparently
doesn't filter this even in the latest module version. I'm not sure of
what issues prevent it from doing that, but for now we'll use a regexp
substitution to escape the period with a backslash.

cerebot/discord.py
@@ -36,6 +36,7 @@ r'(?::\d{2,5})?(?:/[^\s]*)?)')
 
 _emote_regexp = re.compile(r'^<:[^>]+:([0-9]+)>$')
 _channel_regexp = re.compile(r'^<#([0-9]+)>$')
+_list_markdown_regexp = re.compile(r'^([0-9]+)\.')
 
 # String that faction roles end with.
 _faction_suffix = " Faction"
@@ -272,6 +273,9 @@ class DiscordSource(ChatWatcher):
             else:
                 p = discord.utils.escape_markdown(p)
             result += p
+
+        # Escape output interpreted as a markdown ordered list.
+        result = _list_markdown_regexp.sub(r'\1\.', message)
 
         return result
 

commit b47e338351810bbaf4b16520b9ddc61d686141fe

fix: Correct removal of link previews
Broken by 448b7bb3.

cerebot/discord.py
@@ -275,7 +275,7 @@ class DiscordSource(ChatWatcher):
             result += p
 
         # Escape output interpreted as a markdown ordered list.
-        result = _list_markdown_regexp.sub(r'\1\.', message)
+        result = _list_markdown_regexp.sub(r'\1\.', result)
 
         return result
 